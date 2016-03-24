b9c2e1fd41ec62c2630b6c95504d63d474fa5688
frbec <fredrikbeckius@gmail.com>
2016-03-24 16:49:59 +0800

    cleaned up functions.php for theme posifon

--- wp-content/plugins/contact-form-7-simple-hidden-field/README.md
+++
@@ -1,6 +0,0 @@
-Contact Form 7 - Simple Hidden Field
-====================================
-
-Add simple & dynamic hidden fields in Contact Form 7
-
-See readme.txt
--- wp-content/themes/posifon/functions.php
+++ wp-content/themes/posifon/functions.php
@@ -111,17 +111,10 @@ function posifon_scripts() {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
-	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'posifon-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20141010' );
-	}
     // Load bestall-script for selectively hiding some elements of the form
     if ( is_page_template( 'template-bestall.php' )) {
       wp_enqueue_script( 'posifon-bestall', get_template_directory_uri() . '/js/bestall.js', array( 'jquery' ), '20150709', true );
       wp_enqueue_script( 'posifon-pop-up', get_template_directory_uri() . '/js/jquery.bpopup.min.js', array( 'jquery' ), '0.11.0', true );
-    }
-
-    if ( is_page_template( 'template-kunskapsbank.php' )) {
-      wp_enqueue_script( 'posifon-kunskapsbank', get_template_directory_uri() . '/js/kunskapsbank.js', array( 'jquery' ), '20150713', true );
     }
 
     if ( is_page_template( 'startsida.php' )) {
@@ -225,7 +218,6 @@ function new_excerpt_length($length) {
 }
 add_filter('excerpt_length', 'new_excerpt_length');
 
-
 function is_subpage()
 {
 	global $post, $wpdb;
@@ -249,7 +241,6 @@ if (function_exists('register_nav_menus')) {
 	);
 }
 
-
 /**
  * wpi_stylesheet_dir_uri
  * overwrite theme stylesheet directory uri
@@ -257,257 +248,23 @@ if (function_exists('register_nav_menus')) {
  * @see get_stylesheet_directory_uri()
  */
 function wpi_stylesheet_dir_uri($stylesheet_dir_uri, $theme_name){
-
 	$subdir = '/css';
 	return $stylesheet_dir_uri.$subdir;
-
 }
-
 add_filter('stylesheet_directory_uri','wpi_stylesheet_dir_uri',10,2);
 
-
-/*
-Plugin Name: Simple Image Grabber
-Plugin URI: http://bavotasan.com/2009/simple-image-grabber-wordpress-plugin/
-Description: Display one or all images from a post's content.
-Author: c.bavota
-Version: 1.0.3
-Author URI: http://bavotasan.com
-License: GPL2
-*/
-
-/*  Copyright 2012  c.bavota  (email : cbavota@gmail.com)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-/**
- * Retreive one or all images from a post's content and display it.
- *
- * @param   string $content     Source content, for example get_the_content()
- * @param	int $num			Index number of image to retrieve
- * @param	array $num			First parameter can also be an array
- * @param	int $width			Width setting for retrieved image
- * @param	int $height			Height setting for retrieved image
- * @param	string $class		Class setting for retrieve image
- * @param	boolean $permalink	Link image to parent post
- * @param	boolean $echo		Echo or return result
- *
- * @uses	wp_parse_args()		Parse the defaults and array parameters
- * @uses	get_permalink()		Get permalink to parent post
- * @uses	get_the_content()	Get the post content
- *
- * @return	string  Echo or return requests images
- *
- * @author c.bavota
- */
-
-/**
-* usage:
-* The following function will echo the second image from a post (if there is one) with a width of 150px and a height of 200px, the class "alignleft" and no link to the post.
-*`<?php images( '2', '150', '200', 'alignleft', false ); ?>`
-*
-**/
-function images( $content = '', $num = 1, $width = null, $height = null, $class = 'alignleft', $permalink = true, $echo = true ) {
-
-	// Parse all of the defaults and parameters
-	if ( is_array( $num ) ) {
-
-		$defaults = array(
-			'number' => 1,
-			'width' => '',
-			'height' => '',
-			'class' => 'alignleft',
-			'permalink' => true,
-			'echo' => true
-		);
-
-		$args = wp_parse_args( $num, $defaults );
-
-		extract( $args, EXTR_OVERWRITE );
-
-	} else {
-
-		// Fix for number parameter
-		$number = $num;
-
-	}
-
-	// Set $more variable to retrieve full post content
-	global $more;
-	$more = 1;
-
-	// Setup variables according to passed parameters
-	$size = empty( $width ) ? '' : ' width="' . $width . '"';
-	$size = empty( $height ) ? $size : $size . ' height="' . $height . '"';
-	$class = empty( $class ) ? '' : ' class="' . $class . '"';
-	$link = empty( $permalink ) ? '' : '<a href="' . get_permalink() . '">';
-	$linkend = empty( $permalink ) ? '' : '</a>';
-
-	// Number of images in content
-	$count = substr_count( $content, '<img' );
-	$start = 0;
-
-	// Loop through the images
-	for ( $i = 1; $i <= $count; $i++ ) {
-
-		// Get image src
-		$imgBeg = strpos( $content, '<img', $start );
-		$post = substr( $content, $imgBeg );
-		$imgEnd = strpos( $post, '>' );
-		$postOutput = substr( $post, 0, $imgEnd + 1 );
-
-		// Replace width || height || class
-		if ( $width || $height )
-			$replace = array( '/width="[^"]*" /', '/height="[^"]*" /', '/class="[^"]*" /' );
-		else
-			$replace = '/class="[^"]*" /';
-
-		$postOutput = preg_replace( $replace, '', $postOutput );
-
-		$image[$i] = $postOutput;
-
-		$start = $imgBeg + $imgEnd + 1;
-
-	}
-
-	// Go through the images and return/echo according to above parameters
-	if ( ! empty( $image ) ) {
-
-		if ( 'all' == $number ) {
-
-			$x = count( $image );
-			$images = '';
-
-			for ( $i = 1; $i <= $x; $i++ ) {
-
-				if ( stristr( $image[$i], '<img' ) ) {
-
-					$theImage = str_replace( '<img', '<img' . $size . $class, $image[$i] );
-					$images .= $link . $theImage . $linkend;
-
-				}
-
-			}
-
-		} else {
-
-			if ( stristr( $image[$number], '<img' ) ) {
-
-				$theImage = str_replace( '<img', '<img' . $size . $class, $image[$number] );
-				$images = $link . $theImage . $linkend;
-
-			}
-
-		}
-
-		// Reset the $more tag back to zero
-		$more = 0;
-
-		// Echo or return
-		if ( ! empty( $echo ) )
-	    	echo $images;
-	    else
-	    	return $images;
-
-	}
-
-}
-
-
-/**
-*
-* Author: Richie KS
-* Date:   November 2, 2013
-*
-**/
-
-if( !function_exists('get_the_content_strip_images') ):
-  function get_the_content_strip_images ($more_link_text = '', $stripteaser = 0, $more_file = '') {
-    $content = get_the_content($more_link_text, $stripteaser, $more_file);
-    $content = apply_filters('the_content', $content);
-    $content = strip_tags($content, '<h1><h2><h3><h4><p><a><strong><li><ul><tr><td><ol>');
-    return $content;
-  }
-endif;
-
-if( !function_exists('get_field_strip_images') ):
-  function get_field_strip_images ($field_slug = '') {
-    $content = get_field($field_slug);
-    $content = strip_tags($content, '<h1><h2><h3><h4><p><a><strong><li><ul><tr><td><ol>');
-    return $content;
-  }
-endif;
-
-// Prevent Easy Image Gallery from appending gallery to content
-remove_filter( 'the_content', 'easy_image_gallery_append_to_content' );
-
-// Changing excerpt more
- function new_excerpt_more($more) {
-   global $post;
-   return '… <a href="'. get_permalink($post->ID) . '">' . 'Fortsätt läsa &raquo;' . '</a>';
- }
- add_filter('excerpt_more', 'new_excerpt_more');
-
-
-/* Function permalink Thingy
-* https://digwp.com/2009/09/easy-shortcode-permalinks/
-* by Chris Coyier on September 28th, 2009
-*
-* This shortcode can be used in two ways:
-* Basic: <a href="[permalink id=49]">Using without providing text</a>
-*
-* This way you only provide the id parameter and it only returns a URL.
-* This was you can use that URL however you want. For example, if you needed to add a special class name to the link or something (but only occasionally).
-* Providing text: [permalink id=49 text='providing text']
-*
-* This way returns a fully formatted anchor link back, using the text you pass.
-*/
-
-function permalink_thingy($atts) {
-	extract(shortcode_atts(array(
-		'id' => 1,
-		'text' => ""  // default value if none supplied
-    ), $atts));
-
-    if ($text) {
-        $url = get_permalink($id);
-        return "<a href='$url'>$text</a>";
-    } else {
-	   return get_permalink($id);
-	}
-}
-add_shortcode('permalink', 'permalink_thingy');
-
-if ( ! isset( $content_width ) ) {
-	$content_width = 620;
-}
-
-/**
- * Disable the emoji's
- */
+  /* Disable the emoji 's */
 function disable_emojis() {
-	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-	remove_action( 'wp_print_styles', 'print_emoji_styles' );
-	remove_action( 'admin_print_styles', 'print_emoji_styles' );	
-	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
-	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+	remove_action( 'wp_head ', 'print_emoji_detection_script ', 7 );
+	remove_action( 'admin_print_scripts ', 'print_emoji_detection_script ' );
+	remove_action( 'wp_print_styles ', 'print_emoji_styles ' );
+	remove_action( 'admin_print_styles ', 'print_emoji_styles ' );	
+	remove_filter( 'the_content_feed ', 'wp_staticize_emoji ' );
+	remove_filter( 'comment_text_rss ', 'wp_staticize_emoji ' );	
+	remove_filter( 'wp_mail ', 'wp_staticize_emoji_for_email ' );
+	add_filter( 'tiny_mce_plugins ', 'disable_emojis_tinymce ' );
 }
-add_action( 'init', 'disable_emojis' );
+add_action( 'init ', 'disable_emojis ' );
 
 /**
  * Filter function used to remove the tinymce emoji plugin.
@@ -517,7 +274,7 @@ add_action( 'init', 'disable_emojis' );
  */
 function disable_emojis_tinymce( $plugins ) {
 	if ( is_array( $plugins ) ) {
-		return array_diff( $plugins, array( 'wpemoji' ) );
+		return array_diff( $plugins, array( 'wpemoji ' ) );
 	} else {
 		return array();
 	}
